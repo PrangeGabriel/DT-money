111551990891ab8a5ab8d240ebe727ea020139b3
Gabriel Prange <prange.gabriel@gmail.com>
2021-10-14 13:49:16 -0300

    dashboard complete

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
 :root{
   --red: #e52e4d;
   --blue: #5429cc;
+  --green: #33cc95;
 
   --blue-light: #6933ff;
 
@@ -17,7 +18,7 @@ export const GlobalStyle = createGlobalStyle`
 *{
   margin: 0;
   padding: 0;
-  box-sizing: 0;
+  box-sizing: border-box;
 }
 
 //font-size: 16px (desktop) [é uma boa medida]
