5452f25ea54f251d3dfe0f454c145623f9a02a42
Gabriel Prange <prange.gabriel@gmail.com>
2021-10-21 19:27:03 -0300

    axios e miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
 import App from './App';
+
+//cria um backend falso so pra usar dados no frontend
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
